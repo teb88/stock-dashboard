fd82b7ebf026572eb5f56a3b76ec1de4b4b7a6cf
Esteban Gomez <estebanrc88@gmail.com>
2024-03-24 10:32:34 -0300

    update react router prod base path

---
+++ src/constants.ts
@@ -0,0 +1,1 @@
+export const DEPLOYMENT_PATH = '/stock-dashboard';
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import {DEPLOYMENT_PATH} from './constants.ts';
 
-const router = createBrowserRouter([
-  {path: '/', element: <App />},
-  {path: '/details/:symbol', element: <div>ASD</div>},
-]);
+const router = createBrowserRouter(
+  [
+    {path: '/', element: <App />},
+    {path: '/details/:symbol', element: <div>ASD</div>},
+  ],
+  {basename: import.meta.env.MODE === 'production' ? DEPLOYMENT_PATH : '/'}
+);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>

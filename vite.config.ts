0e1264755be4d93929a90760e94d599e3c8755ad
Esteban Gomez <estebanrc88@gmail.com>
2024-03-31 19:07:49 -0300

    add base path customization

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import react from '@vitejs/plugin-react-swc';
 import {defineConfig} from 'vite';
 
+function getBasePath(mode?: string) {
+  if (process.env.BUILD_BASE) {
+    return process.env.BUILD_BASE;
+  } else if (mode) {
+    return mode === 'production' ? '/stock-dashboard' : '/';
+  } else {
+    return '';
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => ({
-  base: mode === 'production' ? '/stock-dashboard' : '/',
+  base: getBasePath(mode),
   plugins: [react()],
 }));
